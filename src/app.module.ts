57f176d02188ec1c4ef057cbd95d54e321ca368d
BanSeok Kim <bans@hutom.co.kr>
2023-05-22 16:25:09 +0900

    refactor: add module to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,18 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { VideosModule } from './videos/videos.module';
 import { UsersModule } from './users/users.module';
+import { VideoTasksModule } from './videoTasks/videoTasks.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { AnnotationsModule } from './annotations/annotations.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/hvat'), //
+    AnnotationsModule,
+    NotificationsModule,
     UsersModule,
     VideosModule,
+    VideoTasksModule,
   ],
   controllers: [],
   providers: [],
