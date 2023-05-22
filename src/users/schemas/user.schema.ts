00281a1c7683edc762346329c0ef0ac4efbb9219
BanSeok Kim <bans@hutom.co.kr>
2023-05-22 17:02:22 +0900

    feat: add types on userSchema Props

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -14,13 +14,13 @@ export type Role = {
 
 @Schema()
 export class User {
-  @Prop()
+  @Prop({ type: String, unique: true, required: true })
   email: string;
 
-  @Prop()
+  @Prop({ type: String, trim: true, required: true })
   password: string;
 
-  @Prop()
+  @Prop({ type: String, required: true })
   name: string;
 
   @Prop({ nullable: true })
@@ -29,26 +29,42 @@ export class User {
   @Prop({ nullable: true })
   image: string;
 
-  @Prop({ type: String, enum: USER_TASK })
+  @Prop({ type: String, enum: USER_TASK, required: true })
   task: USER_TASK;
 
-  @Prop({ type: Object })
+  @Prop({
+    type: Object,
+    validate: {
+      validator: validateRole,
+      message: 'Invalid user role',
+    },
+    required: true,
+  })
   role: Role;
 
-  @Prop({ default: false })
+  @Prop({ type: Boolean, required: true, default: false })
   isAgreed: boolean;
 
-  @Prop({ default: true })
+  @Prop({ type: Boolean, required: true, default: true })
   isActive: boolean;
 
-  @Prop({ default: 0 })
+  @Prop({ type: Number, default: 0 })
   accWorked: number;
 
-  @Prop({ default: 0 })
+  @Prop({ type: Number, default: 0 })
   accTasks: number;
 
   @Prop({ type: Date, nullable: true })
   lastLogin: Date;
+}
+
+/**
+ * - role은 key값으로 "admin", "annotator"만 가질 수 있음
+ * - admin은 다른 role을 가질 수 없음
+ */
+function validateRole(userRole: Role) {
+  const roles = ['admin', 'annotator'];
+  return Object.keys(userRole).every((role) => roles.includes(role));
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
