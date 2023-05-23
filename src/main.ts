b14b137af839a44ed2d9ecb8358ad213b5b6f8a3
BanSeok Kim <bans@hutom.co.kr>
2023-05-23 15:31:10 +0900

    chore: add package - @nestjs/swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerOptions = new DocumentBuilder()
+    .setTitle('h-vat-server renewal test')
+    .setDescription('h-vat-server renewal test REST API')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerOptions);
+  SwaggerModule.setup('docs', app, document, {
+    customSiteTitle: 'h-vat test REST API',
+  });
+
   await app.listen(3000);
   console.log(`h-vat-server is running on: ${await app.getUrl()}`);
 }
