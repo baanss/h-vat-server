4ed99e47c62d58aee661626717c5c36fea75ca35
BanSeok Kim <bans@hutom.co.kr>
2023-05-24 14:41:36 +0900

    docs: add swagger docs about health-check

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
   @Get()
+  @ApiOperation({ summary: 'health-check h-vat-server' })
+  @ApiResponse({
+    status: 200,
+    description: 'OK',
+  })
   async getHello() {
     return '🔥h-vat-server renewal : in progress!🥲';
   }
